371242567b5c0d2e291af4369895863544fe3694
FREEDOM <freewhynane62@gmail.com>
2022-02-21 23:05:57 +0000

    add form

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { AiFillPlayCircle } from 'react-icons/ai';
 import { SiEthereum } from 'react-icons/si';
 import {BsInfoCircle} from 'react-icons/bs';
 
-import {Loader} from './'
+import { TransactionContext } from '../context/TransactionContext';
+import {Loader} from './';
+import { shortenAddress } from '../utils/shortenAddress';
 
 const commonStyles = 'min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white'
 
@@ -20,13 +22,20 @@ const  Input = ({placeholder, name, type, value, handleChange}) => (
 );
 
 const Welcome = () => {
+  const {connectWallet,currentAccount, formData, sendTransaction, handleChange, isLoading } = useContext(TransactionContext);
 
-  const connectWallet = () => {
 
-  };
+  const handleSubmit = (e) => {
+    const {addressTo, amount, keyword, message} = formData;
 
-  const handleSubmit = () => {
+    e.preventDefault();
 
+    if(!addressTo || !amount || !keyword || !message) {
+      alert('Please fill all the fields');
+      return;
+    }
+
+    sendTransaction();
   }
 
   return (
@@ -39,6 +48,7 @@ const Welcome = () => {
           <p className='text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base'>
             Explore the crypto world. Buy and sell cryptocurrencies easily on Krypto.
           </p>
+           {!currentAccount && (
            <button
             type='button'
             onClick={connectWallet}
@@ -47,7 +57,7 @@ const Welcome = () => {
               <p className='text-white text-base font-semibold '>
                 Connect Wallet
               </p>
-           </button>
+           </button>)}
            <div className='grid sm:grid-cols-3 grid-cols-2 w-full mt-10'>
             <div className={`rounded-tl-2xl ${commonStyles}`}>
               Reliability
@@ -81,7 +91,7 @@ const Welcome = () => {
               </div>
               <div >
                 <p className='text-white font-light text-sm'>
-                  Address
+                  {shortenAddress(currentAccount)}
                 </p>
                 <p className='text-white font-semibold text-lg mt-1'>
                   Ethereum
@@ -91,14 +101,14 @@ const Welcome = () => {
           </div>
 
           <div className='p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism'>
-            <Input placeholder="Address To " name="addressTo" type="text" handleChange={() => {}} />
-            <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={() => {}} />
-            <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={() => {}} />
-            <Input placeholder="Enter Message" name="message" type="text" handleChange={() => {}} />
+            <Input placeholder="Address To " name="addressTo" type="text" handleChange={handleChange} />
+            <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={handleChange} />
+            <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={handleChange} />
+            <Input placeholder="Enter Message" name="message" type="text" handleChange={handleChange} />
 
             <div className='h-[1px]' w-full bg-grey-400 my-2 />
 
-            {false  ? (
+            {isLoading  ? (
               <Loader />
             ) : (
               <button
